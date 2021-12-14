f68e243a6777c36f93860b772ff38d2eebebca28
patricioadolfo.castillocalderon <you@example.com>
2021-12-14 19:14:33 +0100

    write on log file

--- ex3/main.c
+++ ex3/main.c
@@ -15,8 +15,24 @@ void storageManager(int pfds[]){
     printf("\nhello, I am a child and my id is %d. My dad is %d\n", my_child_pid,my_parent_pid);
     int numLoops = 5;
     char* message;
-
     close(pfds[0]); // child will not read. 
+
+
+    DBCONN *db;
+    db = init_connection(0);
+    if(db==NULL) return 1;
+    // FILE* fileSensor = fopen("sensor_data","r");
+    // if (fileSensor == NULL ){// I need to add includes for errors
+    //       perror("My error is");
+    // }
+    // insert_sensor_from_file(db,fileSensor );
+    //find_sensor_by_value(db,15.0,callback);
+    //find_sensor_exceed_value(db,24,callback);
+    //find_sensor_after_timestamp(db,1638097863,callback);
+    // fclose(fileSensor);
+    //disconnect(db); 
+
+
     while(--numLoops){
         asprintf(&message, "Hello");
         write(pfds[1],message,strlen(message)+1); // similar to write a file, remeber to add 1 to message for null value at the end of the string.
@@ -33,15 +49,19 @@ void logFileGenerator(int pfds[]){
     int recordNumber=0;
     FILE *fp_log;
     char recBuffer[MAX];
+
+    fp_log = fopen("gateway.log", "w");
+    FILE_ERROR(fp_log, "Couldn't create gateway.log\n");
       do{
             result = read(pfds[0],recBuffer,MAX);
             SYSCALL_ERROR(result);
             if(result>0){
-                printf("\nI got this message from the storageManager :\t%d\t%lu\t%s\n",recordNumber,time(NULL),recBuffer);
+                fprintf(fp_log,"message from storageManager :\t%d\t%lu\t%s\n",recordNumber,time(NULL),recBuffer);
                 recordNumber++;
             }
         }while(result>0);
     result =close(pfds[0]); // parent won't read anymore. 
+    fclose(fp_log);
     SYSCALL_ERROR(result);  
 }
 
@@ -73,20 +93,6 @@ int main (void ){
         printf("\nStorage Manager %d terminated abnormally %d",pid, storageExitStatus);
 
     }
-
-    //DBCONN *db;
-    //db = init_connection(0);
-    //if(db==NULL) return 1;
-    // FILE* fileSensor = fopen("sensor_data","r");
-    // if (fileSensor == NULL ){// I need to add includes for errors
-    //       perror("My error is");
-    // }
-    // insert_sensor_from_file(db,fileSensor );
-    //find_sensor_by_value(db,15.0,callback);
-    //find_sensor_exceed_value(db,24,callback);
-    //find_sensor_after_timestamp(db,1638097863,callback);
-    // fclose(fileSensor);
-    //disconnect(db); 
     return 0;
 }
 
