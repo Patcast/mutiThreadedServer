8be980ed760abdf668f4a63e937bb0a2ddce6afa
patricioadolfo.castillocalderon <you@example.com>
2021-12-15 11:48:23 +0100

    log file updated with actions from db

--- ex3/main.c
+++ ex3/main.c
@@ -4,7 +4,38 @@
 #include "config.h"
 #include "sensor_db.h"
 
+#include <assert.h>
 
+
+enum log_code { CONNECTED_DB = 0, NOT_CONNECTED,FILE_OPEN,FILE_FAILURE};
+typedef enum log_code log_code_t;
+
+void log_code_generator (log_code_t log,int pfds[])
+{ 
+    char* message;  
+  switch( log ) {
+    case  CONNECTED_DB:
+        asprintf(&message, "Connected successfully to db.");
+        
+        break;
+    case NOT_CONNECTED:
+        asprintf(&message, "Failure trying to connect to db.");
+        exit(EXIT_FAILURE);
+        break;   
+    case  FILE_OPEN:
+        asprintf(&message, "File open successfully.");
+        
+        break;
+    case FILE_FAILURE:
+        asprintf(&message, "Failure trying to open file.");
+        perror("My error is");
+        break;           
+    default: // should never come here
+      assert( 1==0 );
+  }
+  write(pfds[1],message,strlen(message)+1);
+  free(message);
+} 
 
 int callback(void *, int, char **, char **);
 
@@ -13,32 +44,32 @@ void storageManager(int pfds[]){
     my_child_pid = getpid();
     my_parent_pid = getppid();
     printf("\nhello, I am a child and my id is %d. My dad is %d\n", my_child_pid,my_parent_pid);
-    int numLoops = 5;
-    char* message;
+    //int numLoops = 5;
+    //char* message;
     close(pfds[0]); // child will not read. 
 
 
     DBCONN *db;
     db = init_connection(0);
-    if(db==NULL) return 1;
-    // FILE* fileSensor = fopen("sensor_data","r");
-    // if (fileSensor == NULL ){// I need to add includes for errors
-    //       perror("My error is");
-    // }
-    // insert_sensor_from_file(db,fileSensor );
-    //find_sensor_by_value(db,15.0,callback);
+    if(db==NULL) log_code_generator(NOT_CONNECTED,pfds);
+    else log_code_generator(CONNECTED_DB,pfds);
+    FILE* fileSensor = fopen("sensor_data","r");
+    if (fileSensor == NULL ) log_code_generator(FILE_FAILURE,pfds);
+    else log_code_generator(FILE_OPEN,pfds);
+    //insert_sensor_from_file(db,fileSensor );
+    find_sensor_by_value(db,15.0,callback);
     //find_sensor_exceed_value(db,24,callback);
     //find_sensor_after_timestamp(db,1638097863,callback);
-    // fclose(fileSensor);
-    //disconnect(db); 
+    fclose(fileSensor);
+    disconnect(db); 
 
-
-    while(--numLoops){
+   /*  while(--numLoops){
         asprintf(&message, "Hello");
         write(pfds[1],message,strlen(message)+1); // similar to write a file, remeber to add 1 to message for null value at the end of the string.
         free(message);
-        sleep(1);
-    }
+        //sleep(1);
+    } */
+
     close(pfds[1]);// this will tell the parent that no more messages will be sent. 
     exit(EXIT_SUCCESS);
 }
@@ -52,7 +83,7 @@ void logFileGenerator(int pfds[]){
 
     fp_log = fopen("gateway.log", "w");
     FILE_ERROR(fp_log, "Couldn't create gateway.log\n");
-      do{
+      do{   
             result = read(pfds[0],recBuffer,MAX);
             SYSCALL_ERROR(result);
             if(result>0){
@@ -83,7 +114,6 @@ int main (void ){
     else{   // Code that woul be executed by the parent
         logFileGenerator(pipeFileDescriptors);
     }
-
     pid = wait(&storageExitStatus);
     SYSCALL_ERROR(pid);
     if(WIFEXITED(storageExitStatus)){ // check if child exited correctly
